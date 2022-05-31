9a717dcf6d05c254b44416dfce952398d0bd1479
Anna Vilnid <anna.vilnid@gmail.com>
2022-05-31 11:03:54 +0200

    31 may

--- freecodcamp.js
+++ freecodcamp.js
@@ -16,3 +16,40 @@ a = 7;
 var b;
 b = a;
 // Only change code below this line
+
+var a = 9;
+
+var myFirstName = "Anna";
+var myLastName = "Vilnid";
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+const sum = 10 + 10;
+
+const difference = 45 - 33;
