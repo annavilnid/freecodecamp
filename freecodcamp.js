52c775d5d50b01fffe9dae1c3d1088a9c6760b22
Anna Vilnid <anna.vilnid@gmail.com>
2022-06-01 17:44:19 +0200

    1 june_1

--- freecodcamp.js
+++ freecodcamp.js
@@ -117,3 +117,35 @@ const myStr = "This is the start. " + "This is the end"; // Change this line
 
 let myStr = "This is the first sentence." ;
 myStr += "This is the second sentence.";
+
+// Only change code below this line
+const myName = "Anna";
+const myStr = "My name is " + myName + " and I am well!";
+
+const someAdjective = "love";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+// Setup
+let myStr = "Jello World";
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+// Setup
+const lastName = "Lovelace";
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
