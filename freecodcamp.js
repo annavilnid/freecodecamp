455e1de50f26c79bebbe9f69bcb6d3e586543ca6
Anna Vilnid <anna.vilnid@gmail.com>
2022-06-02 07:05:31 +0200

    2 june_2

--- freecodcamp.js
+++ freecodcamp.js
@@ -401,3 +401,60 @@ function testLessOrEqual(val) {
   return "More Than 24";
 }
 testLessOrEqual(10);
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+  if (val >= 25 && val <= 50) {
+    return "Yes";
+  }
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
+
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+  // Only change code above this line
+  return "Inside";
+}
+testLogicalOr(15);
+
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
+  }
+  // Only change code above this line
+  return result;
+}
+testElse(4);
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+  return "Between 5 and 10";
+  }
+}
+testElseIf(7);
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+orderMyLogic(7);
