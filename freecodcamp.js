1f4d2bb9ee20bdc9d5ef486c6de7c6e86ff3d78f
Anna Vilnid <anna.vilnid@gmail.com>
2022-06-02 08:30:34 +0200

    2 june_6

--- freecodcamp.js
+++ freecodcamp.js
@@ -591,3 +591,21 @@ function chainToSwitch(val) {
   return answer;
 }
 chainToSwitch(7);
+
+function isLess(a, b) {
+  // Only change code below this line
+    return a < b;
+  // Only change code above this line
+}
+isLess(10, 15);
+
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+if ( a<0 || b<0) {
+  return
+}
+  // Only change code above this line
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+abTest(2,2);
