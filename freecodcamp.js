85d5727e2a55a16441d77a2f5901bfda5456192a
Anna Vilnid <anna.vilnid@gmail.com>
2022-06-03 13:48:17 +0200

    3 june_1

--- freecodcamp.js
+++ freecodcamp.js
@@ -609,3 +609,139 @@ if ( a<0 || b<0) {
   return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 abTest(2,2);
+
+let count = 0;
+function cc(card) {
+  // Only change code below this line
+if (card === 2 || card === 3 || card ===4 || card  === 5 || card === 6)  {
+  count += 1;
+} else if (card === 10 || card === 'J' || card === 'Q' || card === 'K' || card === 'A') {
+  count -= 1;
+} 
+if (count > 0) {
+  return  count + " Bet"
+} else {
+return count + " Hold"
+}
+  // Only change code above this line
+}
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+const myDog = {
+  // Only change code below this line
+  "name": "Max",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["ball", "bird"]
+  // Only change code above this line
+};
+
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
+
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"];   // Change this line
+const drinkValue = testObj["the drink"];    // Change this line
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+
+// Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+// Only change code below this line
+myDog.name = "Happy Coder";
+
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+myDog.bark = "woof"
+
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+// Only change code below this line
+delete myDog.tails
+
+// Setup
+function phoneticLookup(val) {
+  let result = "";
+  // Only change code below this line
+ const lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank",
+  }
+  result=lookup[val]
+  // Only change code above this line
+  return result;
+}
+phoneticLookup("charlie");
+
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+  return obj.hasOwnProperty(checkProp)? obj[checkProp] : "Not Found"
+  // Only change code above this line
+}
+
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
+myMusic.push({
+  "artist": "Daft Punk",
+  "title": "Homework",
+  "release_year": 1997,
+  "formats": [ 
+   "CD", 
+   "Cassette", 
+   "LP"
+  ],
+  "gold": true})
+  
+
