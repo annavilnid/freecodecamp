888400589987e5d2900964867cc8fb3679eff391
Anna Vilnid <anna.vilnid@gmail.com>
2022-06-04 13:35:58 +0200

    4 JUN_1

--- freecodcamp.js
+++ freecodcamp.js
@@ -841,3 +841,33 @@ const myArray = [];
 for (let i = 9; i >= 1; i -= 2) {
   myArray.push(i);
 }
+
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+// Only change code below this line
+let total = 0
+for (let i = 0; i < myArr.length; i++) {
+   total=total+(myArr[i]);
+}
+
+function multiplyAll(arr) {
+  let product = 1;
+  // Only change code below this line
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+    product*=(arr[i][j]);
+  }
+}
+  // Only change code above this line
+  return product;
+}
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+// Setup
+const myArray = [];
+let i = 10;
+// Only change code below this line
+do {
+    myArray.push(i);
+    i++;
+} while (i === 10);
