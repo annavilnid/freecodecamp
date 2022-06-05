16f804d784d9588a1ba4d682b29de55b2f1622ef
Anna Vilnid <anna.vilnid@gmail.com>
2022-06-05 16:40:36 +0200

    5 JUN_1

--- freecodcamp.js
+++ freecodcamp.js
@@ -994,3 +994,45 @@ function checkScope() {
   console.log('Function scope i is: ', i);
   return i;
 }
+
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+s[0] = 2; 
+s[1] = 5;
+s[2] = 7; 
+  // Only change code above this line
+}
+editInPlace();
+
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  // Only change code below this line
+ Object.freeze(MATH_CONSTANTS)
+  // Only change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch(ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+const myConcat = (arr1, arr2) => [...arr1, ...arr2];
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+// Only change code below this line
+const increment = (number, value = 1) => number + value;
+// Only change code above this line
+
+const sum = (...args) => {
+  return args.reduce((a, b) => a + b, 0);
+}
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+arr2 = [...arr1];  // Change this line
+console.log(arr2);
