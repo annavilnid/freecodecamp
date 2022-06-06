7952b985390efec35405375f7fbfd3f40a7afdb8
Anna Vilnid <anna.vilnid@gmail.com>
2022-06-06 20:45:41 +0200

    6 JUN_1

--- freecodcamp.js
+++ freecodcamp.js
@@ -1089,3 +1089,21 @@ const stats = {
 // Only change code below this line
 const half = ({min, max}) => (max + min) / 2.0; 
 // Only change code above this line
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = [];
+  for (let element of arr) {
+    failureItems.push (
+    `<li class="text-warning">${element}</li>`
+  )
+  }
+  // Only change code above this line
+  return failureItems;
+}
+const failuresList = makeList(result.failure);
