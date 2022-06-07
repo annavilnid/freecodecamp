a9e3c9faacf22970b5a8bf7feeee28da921add57
Anna Vilnid <anna.vilnid@gmail.com>
2022-06-07 21:20:59 +0200

    7 JUNE_6

--- freecodcamp.js
+++ freecodcamp.js
@@ -1189,3 +1189,16 @@ import subtract from "./math_functions.js"
 subtract(7,4);
 const makeServerRequest = new Promise((resolve, reject) => {
 });
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+    // Change this line
+  } else {  
+    reject("Data not received");
+    // Change this line
+  }
+});
