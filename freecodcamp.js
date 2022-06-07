f583f90c16376655d75d4287de65c43d173d18c7
Anna Vilnid <anna.vilnid@gmail.com>
2022-06-07 19:53:33 +0200

    7 JUNE_2

--- freecodcamp.js
+++ freecodcamp.js
@@ -1173,10 +1173,10 @@ const lowercaseString = (string) => {
 }
 export { uppercaseString, lowercaseString };
 
-const uppercaseString = (string) => {
-  return string.toUpperCase();
-}
-const lowercaseString = (string) => {
-  return string.toLowerCase()
-}
-export { uppercaseString, lowercaseString };
+import * as stringFunctions from './string_functions.js' 
+// Only change code above this line
+uppercaseString("hello");
+lowercaseString("WORLD!");
+// Only change code above this line
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
