4ee908c657e4e39745c92384609e81511dcd1382
Anna Vilnid <anna.vilnid@gmail.com>
2022-06-07 20:07:46 +0200

    7 JUNE_4

--- freecodcamp.js
+++ freecodcamp.js
@@ -1183,3 +1183,7 @@ stringFunctions.lowercaseString("WORLD!");
 export default function subtract(x, y) {
   return x - y;
 }
+
+import subtract from "./math_functions.js"    
+// Only change code above this line
+subtract(7,4);
