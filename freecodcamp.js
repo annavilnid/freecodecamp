a085258b72738a06eceedefacf31f9b9b1eeb2fb
Anna Vilnid <anna.vilnid@gmail.com>
2022-06-07 21:26:55 +0200

    7 JUNE_7

--- freecodcamp.js
+++ freecodcamp.js
@@ -1202,3 +1202,16 @@ const makeServerRequest = new Promise((resolve, reject) => {
     // Change this line
   }
 });
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer = true;
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+makeServerRequest.then(result => {
+  console.log(result)
+});
