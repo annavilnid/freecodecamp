d472f4b5963592b1d6c3152d8c2363f5012abefa
Anna Vilnid <anna.vilnid@gmail.com>
2022-06-08 22:32:54 +0200

    8 JUN_2

--- freecodcamp.js
+++ freecodcamp.js
@@ -1219,3 +1219,7 @@ makeServerRequest.then(result => {
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result=myRegex.test(myString); // Change this line
+
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
