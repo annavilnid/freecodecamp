3665232b3b0b217edfca443f7ad071fc21bd0193
Anna Vilnid <anna.vilnid@gmail.com>
2022-06-08 22:37:35 +0200

    8 JUN_3

--- freecodcamp.js
+++ freecodcamp.js
@@ -1227,3 +1227,7 @@ let result = waldoRegex.test(waldoIsHiding);
 let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/; // Change this line
 let result = petRegex.test(petString);
+
+let myString = "freeCodeCamp";
+let fccRegex = /freeCodeCamp/i; // Change this line
+let result = fccRegex.test(myString);
