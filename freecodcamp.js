b85b4b2ce113e51f1582554d0fbaf90b86417c4f
Anna Vilnid <anna.vilnid@gmail.com>
2022-06-10 23:34:12 +0200

    10 JUN_4

--- freecodcamp.js
+++ freecodcamp.js
@@ -1259,3 +1259,7 @@ let result = quoteSample.match(myRegex); // Change this line
 let quoteSample = "3 blind mice.";
 let myRegex = /[^aeiou0-9]/gi; // Change this line
 let result = quoteSample.match(myRegex); // Change this line
+
+let difficultSpelling = "Mississippi";
+let myRegex = /s+/g; // Change this line
+let result = difficultSpelling.match(myRegex);
